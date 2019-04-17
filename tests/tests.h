8086d02d878d255a9dd1a1754615c68a38efef5f
Łukasz Piekarski <wookashp98@gmail.com>
2019-04-17 13:51:10 +0200

    modified tests library

--- tests/tests.h
+++ tests/tests.h
@@ -1,8 +1,37 @@
-//
-// Created by Lukasz on 2019-04-17.
-//
+#if !defined(__TESTS_H__)
+#define __TESTS_H__
 
-#ifndef REGEXPR_TESTS_H
-#define REGEXPR_TESTS_H
+#include <cstdio>
+#include <string>
 
-#endif //REGEXPR_TESTS_H
+#define TEST_ASSERTION_FAILED (0)
+#define TEST_SUCCESS (1)
+
+std::string testContext;
+
+const char *test_result[] = {
+        "assertion failed",
+        "success"
+};
+
+#define assert(pred)\
+    if (!(pred)) {\
+        fprintf(stderr,\
+                "%s:%d assertion failed\n",\
+                __func__,\
+                __LINE__\
+                );\
+        return TEST_ASSERTION_FAILED;\
+    }
+
+#define testInfoLog(stream, msg)\
+    if (testContext == __func__) {\
+        fprintf(stream, "\t%s\n",  msg);\
+    } else {\
+        fprintf(stream, "%s: %s\n",  __func__, msg);\
+        testContext = __func__;\
+    }
+
+#define testStatusLog(stream, func) fprintf(stream, "\t%s test result: %s\n\n",  testContext.c_str(), test_result[func()])
+
+#endif // __TESTS_H__
