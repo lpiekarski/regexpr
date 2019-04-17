b054ddee235657251686eb8d050777fec5fa43bf
Łukasz Piekarski <wookashp98@gmail.com>
2019-04-17 14:26:57 +0200

    created constructors test

---
+++ src/regexpr.h
@@ -0,0 +1,8 @@
+//
+// Created by Lukasz on 2019-04-17.
+//
+
+#ifndef REGEXPR_REGEXPR_H
+#define REGEXPR_REGEXPR_H
+
+#endif //REGEXPR_REGEXPR_H
--- tests/dfa_test.cpp
+++ tests/dfa_test.cpp
@@ -1,4 +1,19 @@
-//
-// Created by Lukasz on 2019-04-17.
-//
+#include "tests.h"
 
+#include "../src/dfa.h"
+
+#define TESTS_NUM 2
+
+int test_01() {
+    return TEST_SUCCESS;
+}
+
+int (*tests[TESTS_NUM])(void) = {
+        test_01
+};
+
+int main() {
+    for (auto test_func : tests)
+        testStatusLog(stdout, test_func);
+    return 0;
+}
