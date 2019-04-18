e574d297a561120bbef7b670cee41577c1826538
Łukasz Piekarski <wookashp98@gmail.com>
2019-04-18 11:41:27 +0200

    added run

--- src/dfa.cpp
+++ src/dfa.cpp
@@ -1,7 +1,51 @@
 #include "dfa.h"
 
-DFA::DFA(): n(0), a(""), start(), stop() {}
+DFA::DFA():
+    n(0),
+    a(""),
+    start(0),
+    accepting(),
+    t() {}
 
-DFA::DFA(const Alphabet &a): n(0), a(a), start(), stop() {}
+DFA::DFA(const Alphabet &a):
+    n(0),
+    a(a),
+    start(0),
+    accepting(),
+    t() {}
 
-DFA::DFA(size_t n, const Alphabet &a): n(n), a(a), start(), stop() {}
+DFA::DFA(size_t n, const Alphabet &a):
+    n(n),
+    a(a),
+    start(0),
+    accepting(),
+    t() {
+    std::vector<size_t> v;
+    v.resize(a.size());
+    t.resize(n, v);
+}
+
+DFA::DFA(size_t n,
+         const Alphabet &a,
+         const std::vector<std::vector<size_t>> &t,
+         const std::vector<bool> &accepting):
+         n(n),
+         a(a),
+         start(0),
+         t(t),
+         accepting(accepting) {
+    if (this->accepting.size() != n)
+        throw InvalidAutomaton();
+    if (this->t.size() != n)
+        throw InvalidAutomaton();
+    for (const std::vector<size_t> &v : t)
+        if (v.size() != a.size())
+            throw InvalidAutomaton();
+}
+
+bool DFA::run(const std::string &str) {
+    size_t state = this->start;
+    for (const char &c : str)
+        state = t[state][this->a.charId(c)];
+    return this->accepting[state];
+}
--- src/dfa.h
+++ src/dfa.h
@@ -1,19 +1,30 @@
 #if !defined(__DFA_H__)
 #define __DFA_H__
 
-#include "automata.h"
+#include <vector>
+
+#include "automaton.h"
 
 class DFA {
 private:
     size_t n;
     Alphabet a;
-    size_t start, stop;
+    size_t start;
+    std::vector<bool> accepting;
+    std::vector<std::vector<size_t>> t;
 public:
     DFA();
 
     explicit DFA(const Alphabet &a);
 
     DFA(size_t n, const Alphabet &a);
+
+    DFA(size_t n,
+        const Alphabet &a,
+        const std::vector<std::vector<size_t>> &t,
+        const std::vector<bool> &accepting);
+
+    bool run(const std::string &str);
 };
 
 #endif // __DFA_H__
