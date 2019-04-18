f490416cbf081e078b6174dda57ffcb8c53f248b
Łukasz Piekarski <wookashp98@gmail.com>
2019-04-18 11:39:27 +0200

    made tests run in separate threads

--- tests/tests.h
+++ tests/tests.h
@@ -1,20 +1,27 @@
 #if !defined(__TESTS_H__)
 #define __TESTS_H__
 
+#include <chrono>
 #include <cstdio>
 #include <exception>
+#include <future>
 #include <string>
+#include <thread>
 
 #define TEST_ASSERTION_FAILED (0)
 #define TEST_SUCCESS (1)
 #define TEST_EXCEPTION (2)
+#define TEST_TIMEOUT (3)
+
+#define TIMEOUT_SECONDS (20)
 
 std::string testContext;
 
 const char *test_result[] = {
         "assertion failed",
         "success",
-        "exception"
+        "exception",
+        "timeout"
 };
 
 #define assert(pred)\
@@ -42,12 +49,22 @@ const char *test_result[] = {
 
 #define testInfoLog(stream, msg)\
     if (testContext == __func__) {\
-        fprintf(stream, "\t%s\n",  msg);\
+        fprintf((stream), "\t%s\n",  (msg));\
+        fflush((stream));\
     } else {\
-        fprintf(stream, "%s: %s\n",  __func__, msg);\
+        fprintf((stream), "%s: %s\n",  __func__, (msg));\
+        fflush((stream));\
         testContext = __func__;\
     }
 
-#define testStatusLog(stream, func) fprintf(stream, "\t%s test result: %s\n\n",  testContext.c_str(), test_result[func()])
+#define testStatusLog(stream, func) {\
+        auto __future = std::async(std::launch::async, (func));\
+        size_t __result;\
+        if (__future.wait_for(std::chrono::seconds(TIMEOUT_SECONDS)) != std::future_status::ready)\
+            __result = TEST_TIMEOUT;\
+        else\
+            __result = __future.get();\
+        fprintf((stream), "\t%s test result: %s\n\n",  testContext.c_str(), test_result[__result]);\
+    }
 
 #endif // __TESTS_H__
