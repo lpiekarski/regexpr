24e0c36ad1b743ce079fa926c207e707044ea870
Łukasz Piekarski <wookashp98@gmail.com>
2019-04-18 11:38:25 +0200

    added run test

--- tests/dfa_test.cpp
+++ tests/dfa_test.cpp
@@ -1,7 +1,7 @@
 #include "tests.h"
 #include "../src/regexpr.h"
 
-#define TESTS_NUM 1
+#define TESTS_NUM 2
 
 int test_01() {
     testInfoLog(stdout, "constructors test");
@@ -9,12 +9,46 @@ int test_01() {
     assertNoException(DFA());
     assertNoException(DFA(Alphabet("123456")));
     assertNoException(DFA(1500, Alphabet("123456")));
+    assertNoException(DFA(3,
+                          Alphabet("abc"),
+                          std::vector<std::vector<size_t>>{
+                              std::vector<size_t>{1, 0, 0},
+                              std::vector<size_t>{0, 2, 0},
+                              std::vector<size_t>{2, 2, 2},
+                          },
+                          std::vector<bool>{
+                              false,
+                              false,
+                              true}))
+
+    return TEST_SUCCESS;
+}
+
+int test_02() {
+    testInfoLog(stdout, "run test");
+
+    DFA dfa(3,
+            Alphabet("abc"),
+            std::vector<std::vector<size_t>>{
+                    std::vector<size_t>{1, 0, 0},
+                    std::vector<size_t>{0, 2, 0},
+                    std::vector<size_t>{2, 2, 2},
+            },
+            std::vector<bool>{
+                    false,
+                    false,
+                    true});
+
+    assert(dfa.run("aacabccacabca"));
+    assert(!dfa.run("aacaccacacccacacaccac"));
+    assert(dfa.run("aacaccacacccacaaaaaaaaaacccabccccccacacacacaccac"));
 
     return TEST_SUCCESS;
 }
 
 int (*tests[TESTS_NUM])(void) = {
-        test_01
+        test_01,
+        test_02
 };
 
 int main() {
